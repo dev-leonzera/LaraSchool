9909d160642cf7d58ac5cb6abe4d0291c03f4f01
dev-leonzera <leonandrade22@gmail.com>
2022-12-12 17:30:19 -0300

    first commit

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">{{ __('Users') }}</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <div class="">
+                        <a href="#" class="btn btn-success btn-md mb-2">
+                            <i class="fa fa-plus"></i>
+                            Novo usuário
+                        </a>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-body p-0">
+
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($users as $user)
+                                    <tr>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>
+                                            <a href="#" class="btn btn-primary btn-sm">
+                                                <i class="fa fa-eye"></i>
+                                                Ver
+                                            </a>
+                                            <a href="#" class="btn btn-secondary btn-sm">
+                                                <i class="fa fa-edit"></i>
+                                                Editar
+                                            </a>
+                                            <a href="#" class="btn btn-danger btn-sm">
+                                                <i class="fa fa-eye"></i>
+                                                Inativar
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+
+                        <div class="card-footer clearfix">
+                            {{ $users->links() }}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+@endsection
